5ba0ea3d52c551ea290097119dd80ac4c6b56591
bjoern-vh <bjoern.vh@gmx.de>
2019-08-15 01:19:56 +0200

    More description added

--- examples.sh
+++ examples.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# First source the lib autoloader into your script
 . lib_inc.sh
 
-lib string_trim '  A String with spaces   '
-
+# Call a function of a library with the keyword 'lib' 
+# following the function name without the lib_-Prefix 
+# and optionally a few arguments
+lib string_trim '  A string with spaces   '
