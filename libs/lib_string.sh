0977490ef2d215a986ab99b0d932949897dbcc04
bjoern-vh <bjoern.vh@gmx.de>
2019-08-15 00:41:41 +0200

    Create lib_string.sh
    Simple lib_string_repeat and lib_string_pad functions added. Has to be reviewed.

---
+++ libs/lib_string.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#* function lib_string_repeat
+#* Repeats a character a given times
+#* @arg1 String Character to repeat
+#* @arg2 Int Quantity to repeat
+#* @return String Repeated String
+function lib_string_repeat() {
+    local res=$(printf "${1:0:1}%.0s" {1..$2})
+    echo "$res"
+}
+
+#* function lib_string_pad
+#* Pads a string to a given length with a filling character
+#* @arg1 String Input
+#* @arg2 Int Size 
+#* @arg3 String Character to fill
+#* @arg4 (Optional) String left|right; Default: left
+function lib_string_pad() {
+    local res
+    local spacer=lib_string_repeat $2 $1
+    if [ "$4" != "left" ]; then
+        res="${spacer:0:$(($2 - ${#1}))}${1:0:$2}"            
+    else
+        res="${1:0:$2}${spacer:0:$(($2 - ${#1}))}"
+    fi
+    echo "$res"
+}
